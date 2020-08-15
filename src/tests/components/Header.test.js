a9b6f40063bbf32c1b233158e513fadd4343579d
Raphael <paiva.raphaelt@gmail.com>
2020-08-15 18:23:33 +0200

    create test "header should have an link 'le blog de raphael' and 'about'"

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -9,4 +9,13 @@ describe('component Header', () => {
         const component = render(<Header/>);
         expect(component).not.toBeNull();
     })
+
+    test('header get li about and title', () => {
+        const { getAllByTestId, debug } = render(<Header/>);
+        const shouldHaveLink = ['Le blog de Raphael', 'À propos'];
+
+        const linkElement = getAllByTestId('link-header').map(link => link.textContent);
+
+        expect(linkElement).toEqual(shouldHaveLink);
+    })
 })
