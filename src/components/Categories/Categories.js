04e160f568d57a027c5e7567354939866e6b3c89
Raphael <paiva.raphaelt@gmail.com>
2020-08-15 20:52:20 +0200

    create loading

---
+++ src/components/Categories/Categories.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Categories = () => {
+    return (
+        <>
+            <p data-testid="p-element">Loading ...</p>
+        </>
+    )
+};
+
+export default Categories;
+
