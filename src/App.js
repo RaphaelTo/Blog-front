6db9284d89d51e5a58569a33335f5781ec7706a0
Raphael <paiva.raphaelt@gmail.com>
2020-08-15 17:44:13 +0200

    add structure react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Router>
+        <Switch>
+            <Route exact path="/">
+            </Route>
+            <Route path="/article/:id">
+            </Route>
+            <Route path="/about">
+            </Route>
+            <Route>
+            </Route>
+        </Switch>
+    </Router>
   );
 };
 
