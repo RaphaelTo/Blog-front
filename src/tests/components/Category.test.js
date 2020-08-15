f3f2927b8e1ad67e3dec34caef84cbf9a378657b
Raphael <paiva.raphaelt@gmail.com>
2020-08-15 21:23:43 +0200

    "display all categories"

--- src/tests/components/Category.test.js
+++ src/tests/components/Category.test.js
@@ -6,8 +6,29 @@ import Category from '../../components/Categories/Category';
 
 describe('component Category', () => {
     test('component "Category" exist', () => {
-       const mock = {};
+       const mock = [];
        const cat = render(<Category items={mock}/>);
        expect(cat).not.toBeNull();
     });
+
+    test('display all category', () => {
+        const mockCategories = [
+            {
+                "_id": "5f295cd2c10d1e0622a013e7",
+                "name": "front-end",
+                "__v": 0
+            },
+            {
+                "_id": "5f2960e9130f6307af906be2",
+                "name": "back-end",
+                "__v": 0
+            }
+        ];
+        const { getAllByTestId } = render(<Category items={mockCategories} />);
+
+        const getNameCat = getAllByTestId('name-categories').map(item => item.textContent);
+        const shouldReturn = ['front-end', 'back-end'];
+
+        expect(shouldReturn).toEqual(getNameCat);
+    })
 });
