02aac15c8624defcac8149693b32bbcf8c06b6ec
Raphael <paiva.raphaelt@gmail.com>
2020-08-18 00:54:48 +0200

    create class css

--- src/components/Articles/ArticleItem.js
+++ src/components/Articles/ArticleItem.js
@@ -1,18 +1,19 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import './ArticleItem.css';
 
 const ArticleItem = ({items}) => {
 
     const article = items.map((result, key) => (
-        <div key={key}>
-            <img src={result.img} alt={result.title} data-testid="img-article"/>
-            <span data-testid="title-article">{result.title}</span>
+        <div className="article-item" key={key}>
+            <img src={result.img} alt={result.title} width="350" height="250" data-testid="img-article"/>
+            <h1 data-testid="title-article">{result.title}</h1>
             <span data-testid="abstract-article">{result.abstract}</span>
         </div>
     ));
 
     return (
-        <div>
+        <div className="containt-article">
             {article}
         </div>
     );
