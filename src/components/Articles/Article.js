4e3c56d2264f6f19d9d261ad1ec7055087d7ff4a
Raphael <paiva.raphaelt@gmail.com>
2020-08-17 18:33:35 +0200

    add state, usecallback, useeffect to get articles

--- src/components/Articles/Article.js
+++ src/components/Articles/Article.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
+import axios from 'axios';
+import ArticleItem from "./ArticleItem";
 
 const Article = () => {
+    const [articles, setArticles] = useState({result: []});
+
+    const getArticles = useCallback(async () => {
+       const response = await axios.get('http://localhost:30003/api/v1/article/all');
+       setArticles({result: response.data.result});
+    }, []);
+
+    useEffect(() => {
+        getArticles();
+    },[getArticles]);
+
     return (
-        <h1 data-testid="zero-article">0 article trouvé</h1>
+        <>
+            {articles.result.length > 0 ?
+                    <div data-testid="article-component">
+                        <ArticleItem items={articles.result} />
+                    </div>
+                    :
+                    <h1 data-testid="zero-article">0 article trouvé</h1>
+            }
+        </>
     )
 };
 
