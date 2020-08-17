063852212f054061c0ea40486120867806381f25
Raphael <paiva.raphaelt@gmail.com>
2020-08-17 17:51:54 +0200

    display article

--- src/components/Articles/ArticleItem.js
+++ src/components/Articles/ArticleItem.js
@@ -1,9 +1,30 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
-const ArticleItem = () => {
+const ArticleItem = ({items}) => {
+
+    const article = items.map((result, key) => (
+        <div key={key}>
+            <img src={result.img} alt={result.title} data-testid="img-article"/>
+            <span data-testid="title-article">{result.title}</span>
+            <span data-testid="abstract-article">{result.abstract}</span>
+        </div>
+    ));
+
     return (
-        <h1>hello</h1>
-    )
+        <div>
+            {article}
+        </div>
+    );
+
+};
+
+ArticleItem.propTypes = {
+    items: PropTypes.array
+};
+
+ArticleItem.defaultProps = {
+    items: []
 };
 
 export default ArticleItem;
