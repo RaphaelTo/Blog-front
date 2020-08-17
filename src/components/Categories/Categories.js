865b44b1af80749a1982227c5881b6d34a475a65
Raphael <paiva.raphaelt@gmail.com>
2020-08-17 14:26:32 +0200

    create component categories with method get data from api blog

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import axios from 'axios';
+import Category from "./Category";
 
 const Categories = () => {
+    const [categories, setCategories] = useState({result : []});
+
+    const getCategoriesApi = useCallback( async () => {
+        const response = await axios.get('http://localhost:30003/api/v1/category/all/');
+        setCategories({result : response.data.result} )
+    }, []);
+
+    useEffect(() => {
+        getCategoriesApi()
+    }, []);
+
     return (
         <>
-            <p data-testid="p-element">Loading ...</p>
+            {categories.result.length > 0 ?
+                <div data-testid="category-component">
+                    <Category items={categories.result} />
+                </div>
+                : <p data-testid="p-element">Aucune catégorie</p>}
         </>
     )
 };
