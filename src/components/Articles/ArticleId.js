a4272ecf1627d4ee1cbcad62cca622f38c19a065
Raphael <paiva.raphaelt@gmail.com>
2020-08-19 18:13:29 +0200

    display article

--- src/components/Articles/ArticleId.js
+++ src/components/Articles/ArticleId.js
@@ -1,8 +1,40 @@
-import React from "react";
+import React, {useEffect, useCallback, useState} from "react";
+import axios from 'axios';
+import {useParams} from 'react-router-dom';
 
 const ArticleId = () => {
+
+    const [article, setArticle] = useState({  type: '', result : {} } );
+    const { id } = useParams();
+
+    const convertDate = (date) => {
+        const newDate = new Date(date);
+        return `${newDate.getDate()}-${newDate.getMonth() + 1}-${newDate.getFullYear()}`;
+    };
+
+    const getArticleById = useCallback(async () => {
+        const response = await axios.get(`http://localhost:30003/api/v1/article/byId/${id}`);
+        response.data.result.date = convertDate(response.data.result.date);
+        setArticle(response.data);
+    },[]);
+
+    useEffect(() => {
+        getArticleById();
+    },[getArticleById]);
+
     return (
-        <p data-testid="loading-article">Loading ...</p>
+        <>
+        {
+            article.type === "success" ?
+                <div data-testid="article-content">
+                    <h1 data-testid="article-title">{article.result.title}</h1>
+                    <img data-testid="article-img" src={article.result.img} alt={article.result.title} />
+                    <p data-testid="content">{article.result.content}</p>
+                    <p data-testid="article-date">{article.result.date}</p>
+                </div>
+                : <p data-testid="loading-article">Loading ...</p>
+        }
+        </>
     )
 };
 
