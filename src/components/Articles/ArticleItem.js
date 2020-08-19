a438a49d5c8bd6fc2aa09238859b28eac24d5fba
Raphael <paiva.raphaelt@gmail.com>
2020-08-19 16:35:50 +0200

    add link each article

--- src/components/Articles/ArticleItem.js
+++ src/components/Articles/ArticleItem.js
@@ -1,21 +1,26 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import {BrowserRouter, Link} from "react-router-dom";
 import './ArticleItem.css';
 
 const ArticleItem = ({items}) => {
 
     const article = items.map((result, key) => (
-        <div className="article-item" key={key}>
-            <img src={result.img} alt={result.title} width="350" height="250" data-testid="img-article"/>
-            <h1 data-testid="title-article">{result.title}</h1>
-            <span data-testid="abstract-article">{result.abstract}</span>
-        </div>
+        <Link to={`/article/${result._id}`}>
+            <div className="article-item" key={key}>
+                <img src={result.img} alt={result.title} width="350" height="250" data-testid="img-article"/>
+                <h1 data-testid="title-article">{result.title}</h1>
+                <span data-testid="abstract-article">{result.abstract}</span>
+            </div>
+        </Link>
     ));
 
     return (
-        <div className="containt-article">
-            {article}
-        </div>
+        <BrowserRouter>
+            <div className="containt-article">
+                {article}
+            </div>
+        </BrowserRouter>
     );
 
 };
